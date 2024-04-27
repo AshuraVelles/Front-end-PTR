b1b8bb264c35508f6090b698d1347bbcc6c12886
Ângelo Rodrigo Sousa Bastos <82399046+relito17@users.noreply.github.com>
2024-04-27 17:24:57 +0100

    pagina login

---
+++ src/Login.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import './Login.css';
+
+
+function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
+
+  const handleLogin = () => {
+    // Your login logic here
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+
+    
+    <div className="login-container">
+      <div className="company-name">RCA</div>
+      <div className="welcome-text">Bem-vindos</div>
+      <div className="login-box">
+       
+        <div className="login-title">Login</div>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="input-container">
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="forgot-password">Esqueci-me da palavra-passe</div>
+        </div>
+        <button className="login-enter-button" onClick={handleLogin}>
+          Entrar
+        </button>
+        <div className="bottom-buttons">
+          <button className="register-button">Registar</button>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
