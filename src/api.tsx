29ff635208654e05978f92b8cc7b7b452732aa82
Mona Lisa <tiagogeraldesramalho@gmail.com>
2024-05-09 23:38:31 +0100

    fixed some issues, plus added found items

--- src/api.tsx
+++ src/api.tsx
@@ -1,11 +1,22 @@
 // apiService.tsx
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:4243';
+const BASE_URL = 'http://localhost:3999/v1';
 
 export const fetchLostItems = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/items/lost`);
+    return response.data;
+  } catch (error) {
+    console.error('Failed to fetch items:', error);
+    return []; // Return an empty array in case of error
+  }
+}
+
+
+export const fetchFoundItems = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/items/found`);
     return response.data;
   } catch (error) {
     console.error('Failed to fetch items:', error);
