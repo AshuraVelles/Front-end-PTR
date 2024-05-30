c0836e33e6aca59fc57337f05751d015a6fe1690
Ângelo Rodrigo Sousa Bastos <82399046+relito17@users.noreply.github.com>
2024-05-30 20:13:28 +0100

    register policia quase

--- src/RegisterPolicia.tsx
+++ src/RegisterPolicia.tsx
@@ -1,41 +1,24 @@
 import React, { useState } from 'react';
-import './register.css';
+import './registerPolicia.css';
 import Header from './Components/Header';
 import { useNavigate } from 'react-router-dom';
 
-function RegisterPolicia() {
-  const [username, setUsername] = useState('');
+function RegisterPolice() {
   const [nome, setNome] = useState('');
-  const [genero, setGenero] = useState('');
-  const [data_nasc, setDataNasc] = useState('');
-  const [morada, setMorada] = useState('');
-  const [email, setEmail] = useState('');
-  const [telemovel, setTelemovel] = useState('');
-  const [password, setPassword] = useState('');
+  const [postoPolicia, setPostoPolicia] = useState('');
+  const [historicoPolicia, setHistoricoPolicia] = useState('');
   const [errors, setErrors] = useState({});
-  const [hasErrors, setHasErrors] = useState(false); 
+  const [hasErrors, setHasErrors] = useState(false);
   const navigate = useNavigate();
 
   const validate = () => {
     const newErrors = {};
   
-    if (!username) newErrors.username = 'Username é obrigatório';
     if (!nome) newErrors.nome = 'Nome é obrigatório';
-    if (!genero) newErrors.genero = 'Género é obrigatório';
-    if (!data_nasc) newErrors.data_nasc = 'Data de nascimento é obrigatória';
-    if (!morada) newErrors.morada = 'Morada é obrigatória';
-    if (!email) newErrors.email = 'E-mail é obrigatório';
-    if (!telemovel) {
-      newErrors.telemovel = 'Número de telemóvel é obrigatório';
-    } else if (!/^\d+$/.test(telemovel)) {
-      newErrors.telemovel = 'Introduza apenas dígitos';
-    }
-    if (!password) {
-      newErrors.password = 'Palavra-passe é obrigatória';
-    } else if (password.length < 8) { // Updated minimum length to 8 characters
-      newErrors.password = 'Palavra-passe deve ter pelo menos 8 caracteres';
-    } else if (!/[A-Z]/.test(password) || !/\d/.test(password)) { // Ensure at least one uppercase letter and one number
-      newErrors.password = 'Palavra-passe deve conter pelo menos uma letra maiúscula e um número';
+    if (!postoPolicia) {
+      newErrors.postoPolicia = 'Posto de polícia é obrigatório';
+    } else if (!/^\d+$/.test(postoPolicia)) {
+      newErrors.postoPolicia = 'Introduza apenas dígitos para o posto de polícia';
     }
   
     setErrors(newErrors);
@@ -43,27 +26,20 @@ function RegisterPolicia() {
     setHasErrors(hasErrors);
     return !hasErrors;
   };
-  
 
-  const handleRegisterPolicia = async () => {
+  const handleRegister = async () => {
     if (!validate()) return;
 
     const payload = {
-      username: username,
       nome: nome,
-      genero: genero,
-      data_nasc: data_nasc,
-      morada: morada,
-      email: email,
-      telemovel: telemovel,
-      password: password,
-      ativo: true
+      posto_policia: parseInt(postoPolicia, 10),
+      historico_policia: JSON.parse(historicoPolicia || '{}')
     };
 
     console.log('Payload:', JSON.stringify(payload));
 
     try {
-      const response = await fetch('http://localhost:3995/v1/users/register', {
+      const response = await fetch('http://localhost:3995/police/members', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -74,30 +50,23 @@ function RegisterPolicia() {
       const data = await response.json();
 
       if (response.ok) {
+        alert(`Polícia registrado com sucesso. ID: ${data.id}`);
         navigate('/login');
       } else {
-        console.error('Registration failed:', data.message || data);
+        console.error('Falha no registro:', data.message || data);
       }
     } catch (error) {
-      console.error('Error occurred:', error);
+      console.error('Ocorreu um erro:', error);
     }
   };
 
   return (
     <div className="login-container">
+      
       <div className={`login-box ${hasErrors ? 'error-active' : ''}`}>
-        <div className="login-title">Registar</div>
+        <div className="login-title">Registar Polícia</div>
         <div className="input-container">
           <div className='left'>
-            <label>Escolhe um Username  POLICIA</label>
-            <input
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            {errors.username && <div className="error">{errors.username}</div>}
-
             <label>Nome</label>
             <input
               type="text"
@@ -106,70 +75,31 @@ function RegisterPolicia() {
               onChange={(e) => setNome(e.target.value)}
             />
             {errors.nome && <div className="error">{errors.nome}</div>}
-
-            <label>Género</label>
+            
+            <label>Posto de Polícia</label>
             <input
               type="text"
-              placeholder="Género"
-              value={genero}
-              onChange={(e) => setGenero(e.target.value)}
+              placeholder="ID"
+              value={postoPolicia}
+              onChange={(e) => setPostoPolicia(e.target.value)}
             />
-            {errors.genero && <div className="error">{errors.genero}</div>}
-
-            <label>E-mail</label>
-            <input
-              type="text"
-              placeholder="E-mail"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            {errors.email && <div className="error">{errors.email}</div>}
+            {errors.postoPolicia && <div className="error">{errors.postoPolicia}</div>}
           </div>
           <div className='right'>
-            <label>Data de Nascimento</label>
-            <input
-              type="date"
-              placeholder="Data de Nascimento"
-              value={data_nasc}
-              onChange={(e) => setDataNasc(e.target.value)}
+            <label>Histórico de Polícia</label>
+            <textarea
+              placeholder="Histórico de Polícia (JSON)"
+              value={historicoPolicia}
+              onChange={(e) => setHistoricoPolicia(e.target.value)}
             />
-            {errors.data_nasc && <div className="error">{errors.data_nasc}</div>}
-          
-            <label>Morada</label>
-            <input
-              type="text"
-              placeholder="Morada"
-              value={morada}
-              onChange={(e) => setMorada(e.target.value)}
-            />
-            {errors.morada && <div className="error">{errors.morada}</div>}
-
-            <label>Nº Telemóvel</label>
-            <input
-              type="text"
-              placeholder="Nº Telemóvel"
-              value={telemovel}
-              onChange={(e) => setTelemovel(e.target.value)}
-            />
-            {errors.telemovel && <div className="error">{errors.telemovel}</div>}
-
-            <label>Escolha uma Palavra-Passe</label>
-            <input
-              type="password"
-              placeholder="Palavra-Passe"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {errors.password && <div className="error">{errors.password}</div>}
           </div>
           <div className="bottom-buttons">
-          <button className="register-button" onClick={handleRegisterPolicia}>Criar Conta</button>
+            <button className="register-button" onClick={handleRegister}>Registrar</button>
+          </div>
         </div>
-        </div>
-        
       </div>
     </div>
   );
 }
 
-export default RegisterPolicia;
+export default RegisterPolice;
